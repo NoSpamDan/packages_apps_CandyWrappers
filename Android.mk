b608fbb8810228d31941980361cedd99b6b4d67f
Dan Cartier <nospamdan1@gmail.com>
2018-09-09 23:43:25 -0400

    res: Add new wallpapers from Roger Truttman
    Nice work!
    
    Change-Id: Ic4f80131be836095c98b71da2822898c5644d345

--- Android.mk
+++ Android.mk
@@ -23,21 +23,9 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1080p
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 LOCAL_SDK_VERSION := current
 LOCAL_PACKAGE_NAME := CandyWrappers
 include $(BUILD_PACKAGE)
-
-
-# Static library for target
-# ========================================================
-include $(CLEAR_VARS)
-LOCAL_MODULE := libWallpaperPicker
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_SDK_VERSION := current
-LOCAL_SRC_FILES := $(call all-java-files-under,src)
-LOCAL_JAVA_LIBRARIES := android-support-v4
-include $(BUILD_STATIC_JAVA_LIBRARY)
