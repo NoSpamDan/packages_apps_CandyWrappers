39e092185a5bee4aaa7c830b101af13cbf3c86c7
Dan Cartier <nospamdan1@gmail.com>
2018-09-09 23:16:19 -0400

    CandyWrappers: Rebase for Pie
    Change-Id: Ibaab41c93cf0a6cc8f25f7dc88908535b2124a50

--- Android.mk
+++ Android.mk
@@ -23,8 +23,12 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1080p
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
 LOCAL_SDK_VERSION := current
-LOCAL_PACKAGE_NAME := WallpaperPicker
+LOCAL_PACKAGE_NAME := CandyWrappers
 include $(BUILD_PACKAGE)
 
 
